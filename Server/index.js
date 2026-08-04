85844cf1968ce60bcc834ad167d468ec04e40027
Atchuth619 <atchuth619@gmail.com>
2026-08-04 05:57:49 +0530

    Mongobd setup done

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,17 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 import cors from "cors";
 
-const app = express();
+dotenv.config();
+connectDB();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API Running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.listen(5000, () => console.log("Server started"));
