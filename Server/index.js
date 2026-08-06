8972852cb356c49ea8a2fca77a41e4129475adb8
Atchuth619 <atchuth619@gmail.com>
2026-08-06 10:38:10 +0530

    feat: refactor express app initialization by removing redundant variable declaration

--- Server/index.js
+++ Server/index.js
@@ -9,11 +9,10 @@ import swaggerUi from "swagger-ui-express";
 dotenv.config();
 connectDB();
 
+const app = express();
 app.use(cors({
   origin: "*"
 }));
-
-const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger UI route
