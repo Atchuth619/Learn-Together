55c688fb9fbe5b9889b546db47e5748bd6c2c625
Atchuth619 <atchuth619@gmail.com>
2026-08-13 20:39:19 +0530

    fix: update API baseURL to correct endpoint

--- Client/src/api/authApi.js
+++ Client/src/api/authApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api/entries",  //change it while running it from local
-  baseURL: "https://learn-together-5wns.onrender.com/api/entries",
+  baseURL: "https://learn-together-5wns.onrender.com/api",
 });
 
 export const login = (data) => API.post("/auth/login", data);
