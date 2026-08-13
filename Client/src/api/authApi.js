a589ba68e22c70b514bd71803c902654c5064ed4
Atchuth619 <atchuth619@gmail.com>
2026-08-13 20:26:39 +0530

    api pinting fix

---
+++ Client/src/api/authApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  // baseURL: "http://localhost:5000/api/entries",  //change it while running it from local
+  baseURL: "https://learn-together-5wns.onrender.com/api/entries",
+});
+
+export const login = (data) => API.post("/auth/login", data);
+export const register = (data) => API.post("/auth/register", data);
+export const getMe = () => API.get("/auth/me");
